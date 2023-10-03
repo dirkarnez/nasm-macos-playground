f1b505602b34d5750e34ec92b24369654b5a9c03
Dirk Arnez <smalldirkalex@gmail.com>
2023-10-03 23:32:51 +0800

    - update main.asm

--- main.asm
+++ main.asm
@@ -1,19 +1,24 @@
-section .data
-    hello db 'Hello, World!', 0
+global _main
 
 section .text
-    align 16 ; Set the alignment to 16 bytes
-    global _start
 
-_start:
-    ; write the string to STDOUT
-    mov rax, 1                  ; syscall number for write
-    mov rdi, 1                  ; file descriptor for STDOUT
-    mov rsi, hello              ; address of the string
-    mov rdx, 13                 ; length of the string
+_main:
+    ; amd64abi args passed on rdi, rsi, rdx, r10, r8 and r9
+    ; syscall)
+    ; https://github.com/opensource-apple/xnu/blob/master/bsd/kern/syscalls.master
+    ; 1	AUE_EXIT	ALL	{ void exit(int rval) NO_SYSCALL_STUB; } 
+    ; 4	AUE_NULL	ALL	{ user_ssize_t write(int fd, user_addr_t cbuf, user_size_t nbyte); } 
+    mov     rax, 0x2000004          ; 'write' syscall 4 + 0x20000000
+    mov     rdi, 1                  ; fd=1=stdout
+    mov     rsi, hello_world        ; cbuf
+    mov     rdx, hello_world.len    ; nbyte
     syscall
 
-    ; exit the program
-    mov rax, 60                 ; syscall number for exit
-    xor rdi, rdi                ; exit status code 0
+    mov     rax, 0x2000001          ; 'exit' syscall 4 + 0x20000000
+    mov     rdi, 0                  ; exit code 0
     syscall
+
+section .data
+
+hello_world: db "Hello World!", 10
+.len: equ $ - hello_world

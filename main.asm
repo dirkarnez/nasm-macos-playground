be4feb1e96e3700f8bfc64521faad43e59167d0c
Dirk Arnez <smalldirkalex@gmail.com>
2024-08-31 22:28:35 +0800

    - update main.asm

--- main.asm
+++ main.asm
@@ -8,14 +8,14 @@ _main:
     ; https://github.com/opensource-apple/xnu/blob/master/bsd/kern/syscalls.master
     ; 1	AUE_EXIT	ALL	{ void exit(int rval) NO_SYSCALL_STUB; } 
     ; 4	AUE_NULL	ALL	{ user_ssize_t write(int fd, user_addr_t cbuf, user_size_t nbyte); } 
-    mov     rax, 0x2000004          ; 'write' syscall 4 + 0x20000000
-    mov     rdi, 1                  ; fd=1=stdout
-    mov     rsi, hello_world        ; cbuf
-    mov     rdx, hello_world.len    ; nbyte
+    movq      rax, 0x2000004          ; 'write' syscall 4 + 0x20000000
+    movq      rdi, 1                  ; fd=1=stdout
+    movq      rsi, hello_world        ; cbuf
+    movq      rdx, hello_world.len    ; nbyte
     syscall
 
-    mov     rax, 0x2000001          ; 'exit' syscall 4 + 0x20000000
-    mov     rdi, 0                  ; exit code 0
+    movq      rax, 0x2000001          ; 'exit' syscall 4 + 0x20000000
+    movq      rdi, 0                  ; exit code 0
     syscall
 
 section .data

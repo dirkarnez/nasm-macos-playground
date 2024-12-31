0af5955504775e9643c177f6aac51b489c2e830b
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-31 18:45:59 +0800

    - update main.asm

--- main.asm
+++ main.asm
@@ -1,25 +1,19 @@
 global _main
 
 section .text
-    align 16  ; Ensure proper alignment
+
 _main:
-    ; amd64abi args passed on rdi, rsi, rdx, r10, r8 and r9
-    ; syscall)
-    ; https://github.com/opensource-apple/xnu/blob/master/bsd/kern/syscalls.master
-    ; 1	AUE_EXIT	ALL	{ void exit(int rval) NO_SYSCALL_STUB; } 
-    ; 4	AUE_NULL	ALL	{ user_ssize_t write(int fd, user_addr_t cbuf, user_size_t nbyte); } 
-    mov     rax, 0x2000004          ; 'write' syscall 4 + 0x20000000
-    mov     rdi, 1                  ; fd=1=stdout
+    mov rax, 0x2000004      ; syscall 4: write (
+    mov rdi, 1              ;    fd,
     mov     rsi, hello_world        ; cbuf
     mov     rdx, hello_world.len    ; nbyte
-    syscall
+    syscall                 ; )
 
-    mov     rax, 0x2000001          ; 'exit' syscall 4 + 0x20000000
-    mov     rdi, 0                  ; exit code 0
-    syscall
+    mov     rax, 0x2000001          ; syscall 1: exit (
+    mov     rdi, 0                 ;    retcode
+    syscall    ; )
 
 section .data
-    align 16
 
 hello_world: db "Hello World!", 10
 .len: equ $ - hello_world
